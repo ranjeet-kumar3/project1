6e45d35172874be2937e090e2f8ad0f770ba9e52
ranjeet-kumar3 <ranjeetnandpur7@gmail.in>
2024-10-06 11:40:19 +0530

    page1 done

--- website1.js
+++ website1.js
@@ -1,35 +1,67 @@
-var tl = gsap.timeline();
-tl.from(".line h1", {
-  y: 100,
-  stagger: 0.2,
-  duration: 0.6,
-  delay: 0.4,
-});
-tl.from(".line1-part1, .line h2", {
-  opacity: 0,
-  onStart: function () {
-    var h5timer = document.querySelector(".line1-part1 h5");
-    var grow = 0;
-    setInterval(function () {
-      if (grow < 100) {
-        h5timer.innerHTML = grow++;
-      } else {
-        h5timer.innerHTML = grow;
-      }
-    }, 25);
-  },
-});
-tl.to(".loader", {
-  opacity: 0,
-  duration: 0.3,
-  delay: 2.4,
-});
-tl.from(".page1",{
-    y:1400,
-    opacity:0,
-    duration: 0.8,
-    delay:0.5
+ function textAnimation (){
+
+  var tl = gsap.timeline();
+  tl.from(".line h1", {
+    y: 100,
+    stagger: 0.2,
+    duration: 0.6,
+    delay: 0.4,
+  });
+  tl.from(".line1-part1, .line h2", {
+    opacity: 0,
+    onStart: function () {
+      var h5timer = document.querySelector(".line1-part1 h5");
+      var grow = 0;
+      setInterval(function () {
+        if (grow < 100) {
+          h5timer.innerHTML = grow++;
+        } else {
+          h5timer.innerHTML = grow;
+        }
+      }, 25);
+    },
+  });
+  tl.to(".loader", {
+    opacity: 0,
+    duration: 0.3,
+    delay: 2.4,
+  });
+  tl.from(".page1",{
+      y:1400,
+      opacity:0,
+      duration: 0.8,
+      delay:0.5
+  })
+  tl.to(".loader",{
+      display: "none"
+  })
+  tl.from(".hero  h1", {
+    y: 120,
+    stagger:0.2
+  })
+  
+
+
+}
+textAnimation();
+
+document.addEventListener("mousemove" , function(dets) {
+   gsap.to(".crsr",{
+    left:dets.x,
+    top:dets.y
+   })
+  
 })
-tl.to(".loader",{
-    display: "none"
-})
+
+
+function cursorAnimation(){
+
+  Shery.makeMagnet(".nav-part2 h4" 
+    // ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    // duration: 1,
+  );
+
+}
+cursorAnimation();
+
+
